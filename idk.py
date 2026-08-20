643dc90ba335193cd66e4e994824532f9207a849
StemStuddies <hunter.card@ucas-edu.net>
2026-08-20 09:53:23 -0600

    hmm

--- idk.py
+++ idk.py
@@ -1,6 +1,15 @@
 import time
 import turtle
-for x in range(1,100):
-    turtle.forward(x)
-    turtle.right(10)
+import random
+turt = {
+    1: turtle.Turtle(),
+    2: turtle.Turtle(),
+    3: turtle.Turtle(),
+    4: turtle.Turtle(),
+}
+while True:
+    for y in turt:
+        turt[y].forward(random.randint(0,50))
+        turt[y].right(random.randint(-180,180))
+
 
