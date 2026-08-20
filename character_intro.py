c48dad581f2bf8cfc2f054b12a9b5429bf01a5c9
StemStuddies <hunter.card@ucas-edu.net>
2026-08-20 09:32:44 -0600

    finished character intro

---
+++ character_intro.py
@@ -0,0 +1,6 @@
+#hunter card character_intro.py
+name = input("whats your name? ")
+age = input("how old are you? ")
+job = input("whats your job? ")
+from_were = input("Were are you from? ")
+print(f"wow {name} thats cool! you are {age} and you do {job} for work. And you come from {from_were}.")
