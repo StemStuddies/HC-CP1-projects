1e0e6dedcd809989951fee087265702a7dca4b9c
StemStuddies <hunter.card@ucas-edu.net>
2026-08-26 09:52:30 -0600

    going

---
+++ unit1_final_project.py
@@ -0,0 +1,5 @@
+#hunter card unit1 final project
+name = input("Whats your name")
+age = input("How old are you?")
+print(f"Hello {name} wow you're {age} years old!")
+food = input(f"Wow {name}'s fav")
