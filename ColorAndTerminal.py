5aad9c80be2326eef4779d9f08f4f24af93e275b
StemStuddies <hunter.card@ucas-edu.net>
2026-08-27 09:51:44 -0600

    phgdsoihfdsghfdsoiga

--- ColorAndTerminal.py
+++ ColorAndTerminal.py
@@ -4,8 +4,8 @@ def rgb(r, g, b): #function to set color. using werid string thingys that im goi
     return f"\033[38;2;{r};{g};{b}m"
 def clear(): #for clearing the terminal
     os.system('cls' if os.name == 'nt' else 'clear')
-RESET = "\033[0m"
-print(f" {RESET}")
+reset = "\033[0m"
+print(f" {reset}")
 
 #print(f"{rgb(255, 0, 0)}Red{RESET} {rgb(0, 255, 0)}Green{RESET} {rgb(0, 0, 255)}Blue{RESET}")
 #print(f"{rgb(255, 50, 150)}Custom Neon Pink{RESET}")
