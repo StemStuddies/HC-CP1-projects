5b98689b0c788b8b1ee3821628bccdf1cc439fa8
StemStuddies <hunter.card@ucas-edu.net>
2026-08-27 09:12:48 -0600

    changes

--- unit1_final_project.py
+++ unit1_final_project.py
@@ -1,7 +1,6 @@
 #hunter card unit1 final project
 name = input("Whats your name? ")
 age = input("How old are you? ")
-print(f"Hello {name} wow you're {age} years old!")
 food = input("Whats your favorite food? ")
 color = input("whats your favorite color? ")
-print(f"Wow {name}'s favorite color is {color}, And {name}'s favorite food is {food}.")
+print(f"Wow {name} is {age} years old. {name}'s favorite color is {color}, there favorite food is {food}.")
