859ea2450ab491cf94c4d68b27c7bf717dce128f
StemStuddies <hunter.card@ucas-edu.net>
2026-09-01 09:46:44 -0600

    average grade calculator assignment

--- Average_grade.py
+++ Average_grade.py
@@ -1,9 +1,20 @@
 #hunter card, average grade
-
-classes = int(input("how many classes do you have? \n")) #asking how many classes you have
+while True:
+    try:
+        classes = int(input("how many classes do you have? \n")) #asking how many classes you have
+    except:
+        print("thats not a number")
+    else:
+        break
 grades = [] #used for a list of grades
 for clas in range(classes): #loops for how many classes you have
-    grades.append(int(input(f"Whats youre grade in your {clas + 1} periode: "))) #addes grades to a list 
+    while True:
+        try:
+            grades.append(int(input(f"Whats youre grade in your {clas + 1} periode: "))) #addes grades to a list
+        except:
+            print("thats not a number")
+        else:
+            break    
 print("\n") #new line added for neatness.
 print(f"your average grade is {round(sum(grades)/classes, 2)} %") #calculates and prints the average grade.
 
