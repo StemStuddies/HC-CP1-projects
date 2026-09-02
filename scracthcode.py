10eb61eec77f0de6a63a4a8cf0b71337bd357443
StemStuddies <hunter.card@ucas-edu.net>
2026-09-02 09:50:48 -0600

    changes

--- scracthcode.py
+++ scracthcode.py
@@ -1,2 +1,14 @@
+import time
+import random
+def typew(inputy): #type writter function
+    for x in inputy:
+        print(x, end= "")
+        time.sleep(0.1)
+sentence = "the quick brown fox jumps over the lazy dog"
+start = sentence.find("brown")
 
+print(sentence[start:start+5])
+typew("hello there im a sliding text >:)")
+
+        
 
