7d012f0039eb80f575958003d8ea67a9ff7c9aff
StemStuddies <hunter.card@ucas-edu.net>
2026-09-03 09:42:52 -0600

    new assignment

---
+++ madlib.py
@@ -0,0 +1,9 @@
+#hunter card mad lib.
+name = input("give me a name: ")
+place = input("give me a place: ")
+vehicle = input("give me a vehicle: ")
+verb = input("give me a verb ending in ing: ")
+adjective = input("give me an adjective: ")
+noun = input("give me a noun: ")
+adjective2 = input("give me another adjective: ")
+print("It was only 6 in the morning when " + name + " left to visit " + place + ". First " + name + " rode in a " + vehicle + "! during the trip " + name + " got bored, so " + name + " started " + verb + "." + " Then " + name + " saw a " + adjective + " " + noun + "." + " finaly after a " + adjective2 + " trip " + name + "finaly reached " + place + "." )
