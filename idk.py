38d74dfb1e3d5824013ac6961a2f4b8daaaffa62
StemStuddies <hunter.card@ucas-edu.net>
2026-09-09 09:50:46 -0600

    notes

--- idk.py
+++ idk.py
@@ -1,2 +1,8 @@
-
- 
+message = input("give me a message to encode ")
+codedlet = []
+for x in message:
+    codedlet.append(ord(x))
+message = ""
+for x in codedlet:
+    message += chr(x + 2)
+print(message)
--- notescode.py
+++ notescode.py
@@ -1,6 +1,17 @@
+"""
 letter = input("give me a letter ")
 letter = letter[0].lower()
 number_value = ord(letter)
 number_value += 2
 new_letter = chr(number_value)
 print(f"your letter was {letter} now its {new_letter}")
+"""
+import random
+ducks = random.randint(1,10)
+print(f"There are {ducks} ducks!")
+
+percent = random.random()
+better = percent * 100
+print(f"your random grade is {round(better, 2)}%")
+while True:
+    
