1168251193ed5c3108e04833493a878ac9c0f27d
StemStuddies <hunter.card@ucas-edu.net>
2026-09-22 18:25:13 +0000

    fixxes

--- crew_shares.py
+++ crew_shares.py
@@ -1,22 +1,25 @@
 #hunter card crew share's
 import random as rand
-while True:
+"""while True:
     try:
-        pirates = int(input("how many pirates/crew are on board?"))
+        pirates = int(input("how many pirates/crew are on board? "))
     except:
         print("thats not a number.")
     else:
-        break
+        break """
 startunits = rand.randint(500, 1000)
+pirates = 20
+startunits = 1000
+units = startunits
 units = startunits - pirates * 3
-yondu = round(units * 0.13, 2)
-units -= round(units * 0.13, 2)
-peter = round(units * 0.11, 2)
+yondu = units * 0.13
+units -= yondu
+peter = units * 0.11
 units -= round(units * 0.11, 2)
 crew = units / (pirates + 2)
-yondu += round(crew, 2)
-peter += round(crew, 2)
-crew = round(crew, 2) + 3
+yondu += crew
+peter += crew
+crew = crew + 3
 
 print(f"Starting units: {startunits}")
 print(f"Yondu's Share: {yondu}")
