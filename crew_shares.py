220d610386d872025641ca9e80b77724f1724f27
StemStuddies <hunter.card@ucas-edu.net>
2026-09-23 00:00:12 +0000

    DONE ASSIGNMENT FINALLY

--- crew_shares.py
+++ crew_shares.py
@@ -1,27 +1,26 @@
 #hunter card crew share's
-import random as rand
-"""while True:
+import random as rand #me being lazy.
+while True: 
     try:
         pirates = int(input("how many pirates/crew are on board? "))
     except:
         print("thats not a number.")
     else:
-        break """
-startunits = rand.randint(500, 1000)
-pirates = 20
-startunits = 1000
-units = startunits
-units = startunits - pirates * 3
+        break
+startunits = rand.randint(500, 1000) #its just one line of code?
+units = startunits # seting to make sure it stays the same.
+units = startunits - pirates * 3 #minuses crew 3 from start DOSENT ADD
 yondu = units * 0.13
-units -= yondu
+units -= round(yondu, 2) #rounding to nearest hundredth
 peter = units * 0.11
-units -= round(units * 0.11, 2)
-crew = units / (pirates + 2)
+units -= round(peter, 2)
+crew = units / (pirates + 2) #find out the crew share before the 3 to use as how much everyone gets
 yondu += crew
 peter += crew
-crew = crew + 3
+crew = crew + 3 #adds the 3 that they got for the iorn locaus.
 
-print(f"Starting units: {startunits}")
-print(f"Yondu's Share: {yondu}")
-print(f"Peter's Share: {peter}")
-print(f"Crew's Share: {crew}")
+print(f"Starting units: {round(startunits, 2)}") # display output
+print(f"Yondu's Share: {round(yondu, 2)}")
+print(f"Peter's Share: {round(peter, 2)}")
+print(f"Crew's Share: {round(crew, 2)}")
+
