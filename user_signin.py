a717960b195d1d172d8061e47ad2cd7d902f1cd0
StemStuddies <hunter.card@ucas-edu.net>
2026-09-25 09:24:01 -0600

    finished assignment

--- user_signin.py
+++ user_signin.py
@@ -1,3 +1,13 @@
 #hunter card user sign in
-
-username = input("Whats the username")
+while True:
+    username = input("Whats the username: ")
+    if username == "TheCoolGuy":
+        password = input("whats the password: ")
+        if password == "M3ntT0b5":
+            print("----ACCESS GRANTED----")
+            print("Welcome to the program TheCoolGuy")
+            break
+        else:
+            print("----ACCESS DENIED----")
+    else:
+        print("User not found")
